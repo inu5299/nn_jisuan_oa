e201136a05e5c88e92fc0bdf6e36910f90f03123
Nina_Lee <178659265@qq.com>
2019-10-17 22:33:26 +0800

    main page change
    fix some bug

--- pages/1_main/1_main.vue
+++ pages/1_main/1_main.vue
@@ -71,8 +71,8 @@
 			</view>
 			<view class="oa-node oa-white oa-space_10"  v-for="(item,key) in list"  @click="clickUser(item.Guid)" >
 				 <!-- <task-process :node="item"></task-process> -->
-				 {{item.TaskRate}}
-				<task-process-single :node="item" class="line"  ></task-process-single>
+				<!-- {{item.TaskRate}} -->
+				<task-process-single :node="item" class="line" ></task-process-single>
 			</view>
 					
 		 </view>
